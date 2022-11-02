cc3a54dff2ab0bb6e5fa03f6af955838d76897f2
EgorkaMeau <scarfworks1@gmail.com>
2022-11-02 20:37:43 +0200

    norm

---
+++ JavaScript.js
@@ -0,0 +1,61 @@
+// const arr = [];
+// const arr1 = [1, 2, 3, 4, "salut", "tri", true];
+// console.log(arr);
+// console.log(arr1.length);
+// const user = prompt("cum te numesti")
+// const user1 = prompt("citi ani ai")
+
+// const userArray = []
+// const userArray1 = ["chalupa"]
+
+// userArray.push(userArray1)
+// userArray.push(user)
+// userArray.push(user1)
+// console.log(userArray);
+// let obj = {
+//     name:"Chalupa",
+//     age:"78",
+//     gen:"M",
+// }
+
+// console.log(obj.name, obj.age);
+// console.log(obj);
+
+// let obj_1 = {
+//     name:"Lox",
+//     age:"69",
+//     gen:"F",
+//     NameChild:{
+//         name:"Ben",
+//         age:"5",
+//         gen:"M",
+//     }
+// }
+// console.log(typeof obj_1.NameChild.age);
+
+// const user = [
+//     {name:"Alex"},
+//     {name:"Ion"},
+// ]
+
+// user.forEach(el => {
+//     console.log(el.name);
+// })
+
+const array = [
+    "lu1pa",
+    "pupa",
+    "ronalda",
+    "pepsi",
+    "egorka"
+];
+array.forEach(el => {
+
+    // console.log(el.includes("egor"));
+
+if (el.includes("egor")) {
+    console.log(el);
+}else{
+    console.log("netu")
+}
+})
